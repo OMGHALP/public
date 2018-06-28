66a703efff8669e4bc39015219b3d259a13282d9
Travis Williams <travisjw@gmail.com>
2018-06-28 15:08:13 -0700

    still nothing works.   Getting ready for presentation.  Sheeeit

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,5 +1,7 @@
 'use strict'
 
+//   Local URL  http://127.0.0.1:3000
+//   Heroku URL  https://omg-halp-project.herokuapp.com
 Problem.prototype.insertRecord = function (callback) {
     $.post('http://127.0.0.1:3000/question', {
         question: this.question,
@@ -14,35 +16,29 @@ Problem.prototype.insertRecord = function (callback) {
 }
 
 Problem.fetchAll = callback => {
-    $.get('https://omg-halp-project.herokuapp.com/problem')
+    $.get('http://127.0.0.1:3000/problem')
         .then(results => {
             Problem.loadAll(results);
             callback();
         })
 };
 
-Problem.solve = callback => {
-    $.put('http://127.0.0.1:3000/solutions')
-        .then(
-            console.log('trollololol')
-        )
-}
-
 Problem.prototype.updateRecord = function (callback) {
-    $.put('http://127.0.0.1:3000/question', {
-        question: this.question,
-        tag: this.tag,
-        expectation: this.expectation,
-        result: this.result,
-        best_guess: this.best_guess,
-        code: this.code,
+    console.log(this)
+    $.ajax
+        ({
+            url: `http://127.0.0.1:3000/solution`,
+            method: 'PUT',
+            data: {
+                question: this.question,
+                tag: this.tag,
+                expectation: this.expectation,
+                best_guess: this.best_guess,
+                code: this.code,
+                result: this.result,
+                problem_id: this.id
+            }
     })
         .then(console.log)
         .then(callback);
 }
-// Problem.delete = callback => {
-//     $.ajax shit
-//         .then(
-//             "stuff"
-//         )
-// }
