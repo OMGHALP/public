7a9f79e93cfcdb050d3c994a2059f8dfeaa068ff
Rosie Lara <roselara1@gmail.com>
2018-06-28 15:06:22 -0700

    html updates with button function updates and extra styling added to the submission

--- scripts/index.js
+++ scripts/index.js
@@ -4,98 +4,98 @@
 
 //  Constructor for New problems
 function Problem(problemobj) {
-    this.question = problemobj.question,
-        this.expectation = problemobj.expectation,
-        this.result = problemobj.result,
-        this.best_guess = problemobj.best_guess,
-        this.codes = problemobj.code,
-        this.solved = problemobj.solved,
-        this.tag = problemobj.tag
+  this.question = problemobj.question,
+  this.expectation = problemobj.expectation,
+  this.result = problemobj.result,
+  this.best_guess = problemobj.best_guess,
+  this.codes = problemobj.code,
+  this.solved = problemobj.solved,
+  this.tag = problemobj.tag
 }
 
 Problem.all = [];
 Problem.one = {};
 
 function initMainPage() {
-    //  Hides the Sections for SPA Compliance
-    $("section").hide();
-    $('#animal').hide();
+  //  Hides the Sections for SPA Compliance
+  $("section").hide();
+  $('#animal').hide();
  
-    // Hits the dog.ceo API, gets a random dog pic, and renders it to the page
-    $.ajax("https://dog.ceo/api/breeds/image/random")
-        .then((results) => {
-            $('#animal').attr('src', `${results.message}`);
-        })
+  // Hits the dog.ceo API, gets a random dog pic, and renders it to the page
+  $.ajax("https://dog.ceo/api/breeds/image/random")
+    .then((results) => {
+      $('#animal').attr('src', `${results.message}`);
+    })
 }
 
 $('#provideHelp').on('click', function () {
-    //  User Clicks I'm Here to Help
-    // SPA
-    $("section").hide();
-    $("#animal").hide();
-    $("#browseProblems").empty().append("<h2>Previously Submitted Problems</h2>");
-    $("#browseProblems").show();
+  //  User Clicks I'm Here to Help
+  // SPA
+  $("section").hide();
+  $("#animal").hide();
+  $("#browseProblems").empty().append("<h2>Previously Submitted Problems</h2>");
+  $("#browseProblems").show();
 
-    //Fetches the problems and loads them into Problem.all  Then automatically calls hereToHelp once Problem.all is loaded with problem objects
-    Problem.all = [];
-    Problem.fetchAll(hereToHelp)
+  //Fetches the problems and loads them into Problem.all  Then automatically calls hereToHelp once Problem.all is loaded with problem objects
+  Problem.all = [];
+  Problem.fetchAll(hereToHelp)
 })
 
 function hereToHelp() {
-    Problem.all.forEach(prob => {
-        $('#browseProblems').append(prob.toHtml())
-    })
+  Problem.all.forEach(prob => {
+    $('#browseProblems').append(prob.toHtml())
+  })
 
-    //turns on the event listeners to see what was clicked on.
-    $('problem button').on('click', function (event) {
+  //turns on the event listeners to see what was clicked on.
+  $('problem button').on('click', function (event) {
         
-event.preventDefault();
-        let prob = $(this).parent().find('*')
-        console.log (prob[1].innerText)
-    })
+    event.preventDefault();
+    let prob = $(this).parent().find('*')
+    console.log (prob[1].innerText)
+  })
 }
 
 
 //User clicks on Get Help
 $('#getHelp').on('click', function () {
-    //  SPA navigation
-    // $('#newProblem').empty();
-    $("section").hide();
-    $('#newProblem').show();
-    $("#animal").show();
+  //  SPA navigation
+  // $('#newProblem').empty();
+  $("section").hide();
+  $('#newProblem').show();
+  $("#animal").show();
 });
 
 //User clicks the PLZ HALP button
 $('#submit').on('click', function (event) {
-    event.preventDefault();
-    Problem.one = new Problem({
-        question: $('#problem').val(),
-        expectation: $('#expectation').val(),
-        result: $('#result').val(),
-        best_guess: $('#bestGuess').val(),
-        codes: $('#daCodez').val(),
-        tag: $('#tags').val(),
-    })
+  event.preventDefault();
+  Problem.one = new Problem({
+    question: $('#problem').val(),
+    expectation: $('#expectation').val(),
+    result: $('#result').val(),
+    best_guess: $('#bestGuess').val(),
+    codes: $('#daCodez').val(),
+    tag: $('#tags').val(),
+  })
 
-    Problem.one.insertRecord();
-    initProblemPosted();
+  Problem.one.insertRecord();
+  initProblemPosted();
 });
 
 
 function initProblemPosted() {
-    //SPA "Navigation"
-    $("section").hide();
-    $("#animal").hide();
-    $("#submitted").show();
+  //SPA "Navigation"
+  $("section").hide();
+  $("#animal").hide();
+  $("#submitted").show();
 
 
-    $('#stackoverflow').on('click', function () {
-        window.location.href = "https://stackoverflow.com/questions/ask"
-    })
+  $('#stackoverflow').on('click', function () {
+    window.location.href = "https://stackoverflow.com/questions/ask"
+  })
 
-    $('#home-page').on('click', function () {
-        initMainPage();
-    })
+  $('#home-page').on('click', function () {
+    initMainPage();
+  })
 }
 
 $(document).ready(initMainPage())
@@ -103,13 +103,13 @@ $(document).ready(initMainPage())
 ///   This stuff is from Problem Methods... which is currently not referenced in our HTML
 
 Problem.loadAll = (array) => {
-    array.forEach(element => {
-        Problem.all.push(new Problem(element))
-    })
+  array.forEach(element => {
+    Problem.all.push(new Problem(element))
+  })
 }
 
 Problem.prototype.toHtml = function () {
-    let template = Handlebars.compile($('#problem-template').text());
-    return template(this);
+  let template = Handlebars.compile($('#problem-template').text());
+  return template(this);
 }
 
